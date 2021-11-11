9b9ec60dc2315e33a9b7c85cdffae59a669d0fe2
Tom Allen <tomallen_1@hotmail.co.uk>
2021-11-11 16:58:11 +0000

    added list and bookmark controllers and views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,33 @@
 class ListsController < ApplicationController
+  before_action :get_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list), notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def get_list
+    @list = List.find(params[:id])
+  end
 end
